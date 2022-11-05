2e0ea1a961bc39a29cdab85fd3adcf861ec1fdff
Tolgahan Karabudak <tolgakarabudak2@gmail.com>
2022-11-05 13:56:30 +0300

    Update Main.java

--- Ödevler/Manav_kasa/src/Main.java
+++ Ödevler/Manav_kasa/src/Main.java
@@ -8,21 +8,21 @@ Patlıcan : 5,00 TL*/
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double toplam = 0, geneltoplam = 0;
-        int kilo = 0;
+        double total = 0, generalTotal = 0;
+        int kg = 0;
 
-        String[] fruits = new String[]{"Armut", "Elma", "Domates", "Muz", "Patlıcam"};
-        double[] prices = {2.14, 3.67, 1.11, 0.95, 5.00};
+        String[] fruits = new String[]{"Armut", "Elma", "Domates", "Muz", "Patlıcam"};//string that implements fruits and vagies name
+        double[] prices = {2.14, 3.67, 1.11, 0.95, 5.00};//string that implements prices value
 
         for (int i=0; i< fruits.length; i++)
         {
             System.out.printf(fruits[i] + " Kaç Kilo? : ");
-            kilo = input.nextInt();
-            toplam = kilo * prices[i];
-            geneltoplam +=toplam;
+            kg = input.nextInt();
+            total = kg * prices[i];
+            generalTotal +=total;
 
         }
-        System.out.printf("Toplam Tutar : "+geneltoplam);
+        System.out.printf("Toplam Tutar : "+generalTotal);
 
     }
 }
