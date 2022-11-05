2206c5e34558f88907acd9b0c58579328870385a
Tolgahan Karabudak <tolgakarabudak2@gmail.com>
2022-11-05 13:53:53 +0300

    Update Main.java

--- Ödevler/Vucut_Kitle_endeksi/src/Main.java
+++ Ödevler/Vucut_Kitle_endeksi/src/Main.java
@@ -6,13 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
-        double boy = input.nextDouble();
+        double height = input.nextDouble(); // input height
         System.out.print("Lütfen kilonuzu giriniz : ");
-        double kilo = input.nextDouble();
+        double mass = input.nextDouble();// input mass
 
-        double endeks = kilo / (boy * boy);
+        double endex = mass / (height * height);// formula
 
-        System.out.printf("Vücut kitle endeksiniz : " + endeks);
+        System.out.printf("Vücut kitle endeksiniz : " + endex); // output
 
     }
 }
