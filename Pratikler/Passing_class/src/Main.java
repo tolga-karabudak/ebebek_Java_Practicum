ac1ca35092aa0778c19f0e58a0b042d0243a7e2d
Tolgahan Karabudak <tolgakarabudak2@gmail.com>
2022-11-06 16:43:06 +0300

    https://app.patika.dev/tolgahankarabudak
    https://app.patika.dev/tolgahankarabudak

--- Pratikler/Passing_class/src/Main.java
+++ Pratikler/Passing_class/src/Main.java
@@ -7,18 +7,21 @@ public class Main {
         double point,totalPoint=0;
         double flag = 0;
         String[] lessons = {"Matematik", "Fizik", "Türkçe", "Kimya", "Müzik" };
+
         for (int i=0; i< lessons.length; i++)
         {
             System.out.printf(lessons[i] + " puanınızı giriniz : ");
             point = input.nextInt();
-            if (point<=-1 && point >=101)
+            if (point>0 && point <100)
             {
                 totalPoint = totalPoint + point;
+                flag++;
             }
-            flag++;
+
 
         }
-        System.out.printf("Geçersiz dersleri çıkardığımızdan sonra ortalama : " + (totalPoint)/(flag));
+        System.out.printf("Geçersiz dersleri çıkardığımızdan sonra ortalama  : " + totalPoint / flag );
+
 
 
     }
