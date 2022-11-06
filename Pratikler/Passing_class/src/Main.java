846c7b7021a76195861fdcc9255faee4078406f7
Tolgahan Karabudak <tolgakarabudak2@gmail.com>
2022-11-06 16:25:15 +0300

    Create Main.java

---
+++ Pratikler/Passing_class/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double point,totalPoint=0;
+        double flag = 0;
+        String[] lessons = {"Matematik", "Fizik", "Türkçe", "Kimya", "Müzik" };
+        for (int i=0; i< lessons.length; i++)
+        {
+            System.out.printf(lessons[i] + " puanınızı giriniz : ");
+            point = input.nextInt();
+            if (point<=-1 && point >=101)
+            {
+                totalPoint = totalPoint + point;
+            }
+            flag++;
+
+        }
+        System.out.printf("Geçersiz dersleri çıkardığımızdan sonra ortalama : " + (totalPoint)/(flag));
+
+
+    }
+}
